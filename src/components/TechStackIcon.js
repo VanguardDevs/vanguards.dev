aa318fa76d02ff31a133d5e1203e7a0790ae3d46
Jesus Ordosgoitty <jesuodz@gmail.com>
2022-10-12 00:55:33 -0400

    add icons

--- src/components/TechStackIcon.js
+++ src/components/TechStackIcon.js
@@ -1,19 +1,19 @@
 import * as React from 'react'
 import Box from '@mui/material/Box'
 
-const TechStackIcon = ({ icon, isLarge }) => (
+const TechStackIcon = ({ icon, isLarge, title }) => (
     <Box sx={{
         color: theme => theme.palette.orange,
         margin: '1.5rem',
         height: 'fit-content',
         width: 'fit-content'
-    }}>
+    }} ariaLabel={title} title={title}>
         {React.cloneElement(icon, {
             color: 'inherit',
             sx: {
                 color: 'inherit',
-                width: isLarge ? '8rem' : '4rem',
-                height: isLarge ? '8rem' : '4rem',
+                width: isLarge ? '7rem' : '4rem',
+                height: isLarge ? '7rem' : '4rem',
             }
         })}
     </Box>
--- src/pages/Home/TechStack.js
+++ src/pages/Home/TechStack.js
@@ -13,19 +13,71 @@ import GithubActionsIcon from '../../assets/icons/GithubActionsIcon'
 import CpanelIcon from '../../assets/icons/CpanelIcon'
 import DigitalOceanIcon from '../../assets/icons/DigitalOceanIcon'
 import PWAIcon from '../../assets/icons/PWAIcon';
+import MUIIcon from '../../assets/icons/MUIIcon';
+import SocketIoIcon from '../../assets/icons/SocketIoIcon';
+import DockerIcon from '../../assets/icons/DockerIcon';
 
 const arrayIcons = [
-    <LaravelIcon />,
-    <NodeIcon />,
-    <ReactIcon />,
-    <MongodbIcon />,
-    <PhpIcon />,
-    <PythonIcon />,
-    <PostgresIcon />,
-    <GithubActionsIcon />,
-    <CpanelIcon />,
-    <DigitalOceanIcon />,
-    <PWAIcon />
+    {
+        icon: <LaravelIcon />,
+        title: 'Laravel'
+    },
+    {
+        icon: <NodeIcon />,
+        title: 'Node'
+    },
+    {
+        icon: <NodeIcon />,
+        title: 'Node.js'
+    },
+    {
+        icon: <ReactIcon />,
+        title: 'React.js'
+    },
+    {
+        icon: <MongodbIcon />,
+        title: 'MongoDB'
+    },
+    {
+        icon: <PhpIcon />,
+        title: 'PHP'
+    },
+    {
+        icon: <PythonIcon />,
+        title: 'Python'
+    },
+    {
+        icon: <PostgresIcon />,
+        title: 'PostgresSQL',
+    },
+    {
+        title: 'Github Actions',
+        icon: <GithubActionsIcon />
+    },
+    {
+        title: 'CPANEL',
+        icon: <CpanelIcon />
+    },
+    {
+        title: 'Digital Ocean',
+        icon: <DigitalOceanIcon />
+    },
+    {
+        title: 'Progressive Web Applications',
+        icon: <PWAIcon />
+    },
+    {
+        title: 'MUI (Material UI)',
+        icon: <MUIIcon />
+    },
+    {
+        title: 'Socket.IO',
+        icon: <SocketIoIcon />
+    },
+    {
+        title: 'Docker',
+        icon: <DockerIcon />
+    }
 ]
 
 const TechStack = () => {
@@ -57,7 +109,13 @@ const TechStack = () => {
                 alignItems: 'center',
                 width: '80%'
             }}>
-                {arrayIcons.map(icon => <TechStackIcon icon={icon} isLarge={matches} />)}
+                {arrayIcons.map(item =>
+                    <TechStackIcon
+                        icon={item.icon}
+                        title={item.title}
+                        isLarge={matches}
+                    />
+                )}
             </Box>
         </Box>
     );
